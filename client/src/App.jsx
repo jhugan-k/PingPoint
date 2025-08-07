303455e29d89fbed37a8e23eb7ab9cab2a0665ff
Jhugan Kartikey <jkartikey.official@gmail.com>
2025-08-07 10:36:58 +0530

    Initialised project

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    // These are Tailwind classes!
+    <div className="bg-slate-900 min-h-screen flex items-center justify-center text-center">
+      <h1 className="text-5xl font-extrabold text-white">
+        Hello, Tailwind! <br />
+        <span className="bg-gradient-to-r from-cyan-400 to-blue-600 bg-clip-text text-transparent">
+          Frontend Setup is Complete.
+        </span>
+      </h1>
+    </div>
+  )
+}
+
+export default App
